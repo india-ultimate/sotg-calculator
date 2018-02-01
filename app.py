ca69e78754465ab6d7025b80efc07d1b3309cf5a
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-02-01 11:26:25 +0530

    Add usage information to the index page

--- app.py
+++ app.py
@@ -1,26 +1,39 @@
 #!/usr/bin/env python
 
+from os.path import abspath, dirname, join
 from flask import Flask, render_template, request
+import mistune
 
 from scorer import SOTGScorer
 
+HERE = dirname(abspath(__file__))
+README = join(HERE, 'README.md')
 app = Flask(__name__)
 
 
-def parse_column_args():
+def get_usage():
+    """Return rendered HTML of the Usage section from README.md."""
+    usage = ''
+    for line in open(README):
+        if not usage and not line.startswith('## Usage'):
+            continue
+        elif usage and line.startswith('## ') and not line.startswith('## Usage'):
+            break
+        else:
+            usage += line
+    return mistune.markdown(usage)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    url = request.args.get('url')
     columns = {
         'team': request.args.get('team'),
         'opponent': request.args.get('opponent'),
         'team-score-columns': request.args.getlist('team-score-columns'),
         'opponent-score-columns': request.args.getlist('opponent-score-columns'),
     }
-    return columns
 
-
-@app.route('/', methods=['GET'])
-def index():
-    url = request.args.get('url')
-    columns = parse_column_args()
     if url is None:
         rankings = None
         all_columns = []
@@ -41,6 +54,7 @@ def index():
         all_columns = list(scorer.data.columns)
 
     return render_template('index.html.jinja',
+                           usage=get_usage(),
                            columns=columns,
                            all_columns=all_columns,
                            url=url,

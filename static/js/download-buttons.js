0e7e0c0fabd06e7af846e24487ec559c4ae39f78
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-05-17 08:55:21 +0530

    Add download buttons for downloading scores awarded/received tabs

---
+++ static/js/download-buttons.js
@@ -0,0 +1,32 @@
+var export_png = function(e, tab) {
+  e.preventDefault();
+  e.stopPropagation();
+  $(e.target).remove();
+  var table = tab.querySelector(".table-responsive");
+  domtoimage
+    .toPng(tab, { quality: 0.95, bgcolor: "#ffffff", width: table.scrollWidth })
+    .then(function(dataUrl) {
+      var link = $("<a>")
+        .text("Download")
+        .attr("href", dataUrl)
+        .attr("download", "rankings.png")
+        .appendTo(tab);
+    });
+};
+
+$(function() {
+  // Download button for rankings table
+  var table = document.querySelector("#nav-rankings > .table-responsive");
+
+  var score_pages = ["#nav-rankings", "#nav-scores-received", "#nav-scores-awarded"];
+  score_pages.forEach(function(selector) {
+    var tab = document.querySelector(selector);
+    var link = $("<a>")
+      .text("Create PNG")
+      .attr("href", "#")
+      .appendTo(tab)
+      .click(function(e) {
+        return export_png(e, tab);
+      });
+  });
+});

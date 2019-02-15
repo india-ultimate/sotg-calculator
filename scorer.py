3adfad1c7db577a9323f45f0eb3e475a21dc9205
Puneeth Chaganti <punchagan@muse-amuse.in>
2019-02-15 15:13:24 +0530

    Format scorer with latest version of black

--- scorer.py
+++ scorer.py
@@ -4,25 +4,25 @@ from urllib.parse import urlparse
 import pandas as pd
 import requests
 
-TEAM_COLUMN = 'Your Team'
-OPPONENT_COLUMN = 'Opponent Team'
+TEAM_COLUMN = "Your Team"
+OPPONENT_COLUMN = "Opponent Team"
 OPPONENT_SCORE_COLUMNS = [
-    'Rules Knowledge and Use',
-    'Fouls and Body Contact',
-    'Fair-Mindedness',
-    'Positive Attitude and Self-Control',
-    'Communication',
+    "Rules Knowledge and Use",
+    "Fouls and Body Contact",
+    "Fair-Mindedness",
+    "Positive Attitude and Self-Control",
+    "Communication",
 ]
 TEAM_SCORE_COLUMNS = [
-    'Rules Knowledge and Use (self)',
-    'Fouls and Body Contact (self)',
-    'Fair-Mindedness (self)',
-    'Positive Attitude and Self-Control (self)',
-    'Communication (self)',
+    "Rules Knowledge and Use (self)",
+    "Fouls and Body Contact (self)",
+    "Fair-Mindedness (self)",
+    "Positive Attitude and Self-Control (self)",
+    "Communication (self)",
 ]
-DAY_COLUMN = 'Day'
-TOTAL_SCORE_COLUMN = 'Score'
-TOTAL_SELF_SCORE_COLUMN = 'Self Score'
+DAY_COLUMN = "Day"
+TOTAL_SCORE_COLUMN = "Score"
+TOTAL_SELF_SCORE_COLUMN = "Self Score"
 
 
 def to_numbers(x):
@@ -39,7 +39,7 @@ def to_numbers(x):
 
 def requires_login(url):
     response = requests.get(url)
-    return response.url.startswith('https://accounts.google.com')
+    return response.url.startswith("https://accounts.google.com")
 
 
 class InvalidURLException(Exception):
@@ -48,8 +48,9 @@ class InvalidURLException(Exception):
 
 class SOTGScorer:
     """A class to do all the spirit scoring"""
-    NETLOC = 'docs.google.com'
-    PATH_PREFIX = '/spreadsheets/d/'
+
+    NETLOC = "docs.google.com"
+    PATH_PREFIX = "/spreadsheets/d/"
 
     def __init__(self, url, columns=None):
         self.url = self.get_export_url(url)
@@ -60,65 +61,65 @@ class SOTGScorer:
         """Return the export URL from a spreadsheet URL."""
         parsed = urlparse(url)
         if not parsed.netloc == cls.NETLOC:
-            raise InvalidURLException('Not a google spreadsheet URL')
+            raise InvalidURLException("Not a google spreadsheet URL")
 
         if not parsed.path.startswith(cls.PATH_PREFIX):
-            raise InvalidURLException('Not a google spreadsheet URL')
+            raise InvalidURLException("Not a google spreadsheet URL")
 
         if requires_login(url):
             raise InvalidURLException(
-                'Spreadsheet is not accessible without login'
+                "Spreadsheet is not accessible without login"
             )
 
-        key = parsed.path.split('/')[3]
-        return 'https://{netloc}{path}{key}/export?format=csv'.format(
+        key = parsed.path.split("/")[3]
+        return "https://{netloc}{path}{key}/export?format=csv".format(
             netloc=cls.NETLOC, path=cls.PATH_PREFIX, key=key
         )
 
     @property
     def data(self):
         """Return the data as a Pandas DataFrame."""
-        if not hasattr(self, '_data'):
+        if not hasattr(self, "_data"):
             self._data = pd.read_csv(self.url)
             COLUMNS = self._data.columns
             columns = self.columns
             self.team_column = (
-                COLUMNS[int(columns.get('team'))] if columns.get(
-                    'team'
-                ) else TEAM_COLUMN
+                COLUMNS[int(columns.get("team"))]
+                if columns.get("team")
+                else TEAM_COLUMN
             )
             self.team_score_columns = (
                 [
                     COLUMNS[int(column)]
-                    for column in columns.get('team-score-columns')
-                ] if columns.get(
-                    'team-score-columns'
-                ) else TEAM_SCORE_COLUMNS
+                    for column in columns.get("team-score-columns")
+                ]
+                if columns.get("team-score-columns")
+                else TEAM_SCORE_COLUMNS
             )
             self.opponent_column = (
-                COLUMNS[int(columns.get('opponent'))] if columns.get(
-                    'team'
-                ) else OPPONENT_COLUMN
+                COLUMNS[int(columns.get("opponent"))]
+                if columns.get("team")
+                else OPPONENT_COLUMN
             )
             self.opponent_score_columns = (
                 [
                     COLUMNS[int(column)]
-                    for column in columns.get('opponent-score-columns')
-                ] if columns.get(
-                    'opponent-score-columns'
-                ) else OPPONENT_SCORE_COLUMNS
+                    for column in columns.get("opponent-score-columns")
+                ]
+                if columns.get("opponent-score-columns")
+                else OPPONENT_SCORE_COLUMNS
             )
             self.day_column = (
-                COLUMNS[int(columns.get('day'))] if columns.get(
-                    'day'
-                ) else DAY_COLUMN
+                COLUMNS[int(columns.get("day"))]
+                if columns.get("day")
+                else DAY_COLUMN
             )
         return self._data
 
     @property
     def teams(self):
         """Return team names from the data."""
-        if not hasattr(self, '_teams'):
+        if not hasattr(self, "_teams"):
             team = self.team_column
             opponent = self.opponent_column
             data = self.data
@@ -154,37 +155,40 @@ class SOTGScorer:
         avg_self_score = self_score / self_score_matches
         # Create dataframe to use for ranking
         rankings = pd.DataFrame([score, self_score], dtype=d_int).transpose()
-        rankings['Avg spirit score'] = avg_score
-        rankings['Avg self spirit score'] = avg_self_score
-        rankings['Difference'] = avg_score - avg_self_score
+        rankings["Avg spirit score"] = avg_score
+        rankings["Avg self spirit score"] = avg_self_score
+        rankings["Difference"] = avg_score - avg_self_score
         # Compute and order by ranks
         # FIXME: This is such a mess!
-        rankings = rankings.sort_values('Avg spirit score', ascending=False)
-        ranks = rankings['Avg spirit score'].rank(
-            method='min', ascending=False
+        rankings = rankings.sort_values("Avg spirit score", ascending=False)
+        ranks = rankings["Avg spirit score"].rank(
+            method="min", ascending=False
         )
-        rankings['Rank'] = pd.Series(ranks, dtype=d_int)
-        rankings['Team'] = rankings.index
+        rankings["Rank"] = pd.Series(ranks, dtype=d_int)
+        rankings["Team"] = rankings.index
         column_order = [
-            'Rank',
-            'Team',
-            'Score',
-            'Self Score',
-            'Avg spirit score',
-            'Avg self spirit score',
-            'Difference',
+            "Rank",
+            "Team",
+            "Score",
+            "Self Score",
+            "Avg spirit score",
+            "Avg self spirit score",
+            "Difference",
         ]
         rankings = rankings[column_order]
         # Styling
-        rankings = rankings.style.set_precision('4').set_table_attributes(
-            'border="0" class="dataframe table table-hover table-striped"'
-        ).set_table_styles(
-            [
-                dict(selector=".row_heading", props=[("display", "none")]),
-                dict(selector=".blank", props=[("display", "none")]),
-            ]
-        ).apply(
-            self._bold_columns, axis=0
+        rankings = (
+            rankings.style.set_precision("4")
+            .set_table_attributes(
+                'border="0" class="dataframe table table-hover table-striped"'
+            )
+            .set_table_styles(
+                [
+                    dict(selector=".row_heading", props=[("display", "none")]),
+                    dict(selector=".blank", props=[("display", "none")]),
+                ]
+            )
+            .apply(self._bold_columns, axis=0)
         )
         return rankings
 
@@ -212,9 +216,9 @@ class SOTGScorer:
     def _bold_columns(self, column):
         """Set font-weight if column needs to be bold"""
         return [
-            'font-weight: 700;' if column.name in {
-                'Rank', 'Team', 'Avg spirit score'
-            } else ''
+            "font-weight: 700;"
+            if column.name in {"Rank", "Team", "Avg spirit score"}
+            else ""
             for _ in column
         ]
 
@@ -225,23 +229,23 @@ class SOTGScorer:
         received.
 
         """
-        columns = [
-            self.team_column, self.day_column
-        ] + self.opponent_score_columns + [
-            TOTAL_SCORE_COLUMN
-        ]
+        columns = (
+            [self.team_column, self.day_column]
+            + self.opponent_score_columns
+            + [TOTAL_SCORE_COLUMN]
+        )
         scores = self.data[self.data[self.opponent_column] == team][columns]
-        team_columns = [
-            self.opponent_column, self.day_column
-        ] + self.team_score_columns + [
-            TOTAL_SELF_SCORE_COLUMN
-        ]
+        team_columns = (
+            [self.opponent_column, self.day_column]
+            + self.team_score_columns
+            + [TOTAL_SELF_SCORE_COLUMN]
+        )
         team_scores = self.data[self.data[self.team_column] == team][
             team_columns
         ]
         merged_scores = scores.merge(
             team_scores,
-            how='outer',
+            how="outer",
             left_on=[self.team_column, self.day_column],
             right_on=[self.opponent_column, self.day_column],
         )
@@ -249,15 +253,15 @@ class SOTGScorer:
         merged_scores[self.team_column] = merged_scores[self.team_column].mask(
             pd.isna, merged_scores[self.opponent_column]
         )
-        columns = [
-            self.team_column, self.day_column
-        ] + self.opponent_score_columns + [
-            TOTAL_SCORE_COLUMN
-        ] + self.team_score_columns + [
-            TOTAL_SELF_SCORE_COLUMN
-        ]
+        columns = (
+            [self.team_column, self.day_column]
+            + self.opponent_score_columns
+            + [TOTAL_SCORE_COLUMN]
+            + self.team_score_columns
+            + [TOTAL_SELF_SCORE_COLUMN]
+        )
         display_scores = merged_scores[columns].rename(
-            columns={self.team_column: 'Scored by'}
+            columns={self.team_column: "Scored by"}
         )
         return display_scores
 
@@ -269,39 +273,37 @@ class SOTGScorer:
         scores awarded
 
         """
-        columns = [
-            self.opponent_column, self.day_column
-        ] + self.opponent_score_columns + [
-            TOTAL_SCORE_COLUMN
-        ]
+        columns = (
+            [self.opponent_column, self.day_column]
+            + self.opponent_score_columns
+            + [TOTAL_SCORE_COLUMN]
+        )
         scores = self.data[self.data[self.team_column] == team][columns]
-        team_columns = [
-            self.team_column, self.day_column
-        ] + self.team_score_columns + [
-            TOTAL_SELF_SCORE_COLUMN
-        ]
+        team_columns = (
+            [self.team_column, self.day_column]
+            + self.team_score_columns
+            + [TOTAL_SELF_SCORE_COLUMN]
+        )
         team_scores = self.data[self.data[self.opponent_column] == team][
             team_columns
         ]
         merged_scores = scores.merge(
             team_scores,
-            how='outer',
+            how="outer",
             left_on=[self.opponent_column, self.day_column],
             right_on=[self.team_column, self.day_column],
         )
         # Replace NaN in team column with names from opponent column (self scores)
         merged_scores[self.opponent_column] = merged_scores[
             self.opponent_column
-        ].mask(
-            pd.isna, merged_scores[self.team_column]
+        ].mask(pd.isna, merged_scores[self.team_column])
+        columns = (
+            [self.opponent_column, self.day_column]
+            + self.opponent_score_columns
+            + [TOTAL_SCORE_COLUMN]
+            + self.team_score_columns
+            + [TOTAL_SELF_SCORE_COLUMN]
         )
-        columns = [
-            self.opponent_column, self.day_column
-        ] + self.opponent_score_columns + [
-            TOTAL_SCORE_COLUMN
-        ] + self.team_score_columns + [
-            TOTAL_SELF_SCORE_COLUMN
-        ]
         return merged_scores[columns]
 
     def _make_scores_numbers(self):
